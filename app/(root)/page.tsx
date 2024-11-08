7c2220064781981a3b4c452f1117b9edf3109eaa
nerdboi008 <mail.nerdboi@gmail.com>
2024-11-08 20:09:10 +0530

    implements -hero heading section -sidebar nav for large screens -sidesheet nav for mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
+
+  const loggedIn = {firstName: 'Moin'}
+
   return (
-    <p>Home</p>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+            <HeaderBox
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manage your account and transactions efficiently."
+          />
+          
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={12203.34}
+          />
+        </header>
+        
+      </div>
+    </section>
   );
 }
